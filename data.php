d8b0faef569547e2d663d4ea6590dc2b34a4b113
Komal Rathore <komal.rathore40@gmail.com>
2014-05-18 20:03:44 +0530

    Create data.php
    place it in touch folder.It contain the JSON formatted code.This file is supportive for DataStoreFromPHP.* files

---
+++ data.php
@@ -0,0 +1,10 @@
+<?php
+  echo '{ 
+	  "success": true,
+	  "users"  : [ 
+	    { "name":"Jill" , "age":"40", "id":"2" },
+	    { "name":"Susan", "age":"65", "id":"3" },
+	    { "name":"Pete" , "age":"36", "id":"4" }
+	  ]
+	}';
+?>
